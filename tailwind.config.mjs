78565e0ac94248e3e8233be41be44ad71570eea3
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-03-14 04:47:20 +0100

    Build the navbar

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      container: {
+        center: true,
+        padding: "1.3rem",
+        screens: {
+          sm: "100%",
+          md: "720px",
+          // "2xl": "768px",
+        },
+      },
+      colors: {
+        primary: {
+          DEFAULT: "#FF0000",
+          light: "",
+          dark: "#FFC2C2",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
